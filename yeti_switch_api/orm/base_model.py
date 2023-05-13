25af41cfd3841d447f731cd5af28a9fd7017b766
Denis Talakevich <senid231@gmail.com>
2023-05-13 12:12:05 +0300

    add creatable_fields and updatable_fields

--- yeti_switch_api/orm/base_model.py
+++ yeti_switch_api/orm/base_model.py
@@ -28,3 +28,40 @@ class BaseModel(ApiModel):
                 relationships=relationships,
             )
         )
+
+    def create(self):
+        api_response = self._options.api.endpoint(self.endpoint_path()).post(object=self.__raw_object_for_create())
+        if api_response.status_code == 201:
+            self.raw_object = api_response.content.data
+
+    def update(self):
+        api_response = self.endpoint.patch(object=self.__raw_object_for_update())
+        if api_response.status_code == 200 and api_response.content.data:
+            self.raw_object = api_response.content.data
+
+    def creatable_fields(self):
+        return self.__class__._options.fields.keys()
+
+    def updatable_fields(self):
+        return self.__class__._options.fields.keys()
+
+    def __raw_object_for_update(self):
+        updatable_fields = self.updatable_fields()
+        attributes = {k: v for k, v in self.raw_object.attributes.items() if k in updatable_fields}
+        relationships = {k: v for k, v in self.raw_object.relationships.items() if k in updatable_fields}
+        return JsonApiObject(
+            type=self.type,
+            id=self.id,
+            attributes=attributes,
+            relationships=relationships,
+        )
+
+    def __raw_object_for_create(self):
+        creatable_fields = self.creatable_fields()
+        attributes = {k: v for k, v in self.raw_object.attributes.items() if k in creatable_fields}
+        relationships = {k: v for k, v in self.raw_object.relationships.items() if k in creatable_fields}
+        return JsonApiObject(
+            type=self.type,
+            attributes=attributes,
+            relationships=relationships,
+        )
--- yeti_switch_api/orm/contractor.py
+++ yeti_switch_api/orm/contractor.py
@@ -15,3 +15,19 @@ class Contractor(BaseModel):
     phones = AttributeField("phones")
     external_id = AttributeField("external-id")
     smtp_connection = RelationField("smtp-connection")
+
+    def creatable_fields(self):
+        return [
+            'name',
+            'enabled',
+            'vendor',
+            'customer',
+            'description',
+            'address',
+            'phones',
+            'external_id',
+            'smtp_connection'
+        ]
+
+    def updatable_fields(self):
+        return self.creatable_fields()
