3a4b1032c4965d8b4d6c6871e8f3c8a2ef02f96a
Dmitry <dmitry.sinina@onat.edu.ua>
2023-12-29 19:19:06 +0200

    fix

--- yeti_switch_api/orm/billing/invoice.py
+++ yeti_switch_api/orm/billing/invoice.py
@@ -23,7 +23,9 @@ class Invoice(BaseModel):
     originated_amount_spent = AttributeField("originated-amount-spent")
     originated_amount_earned = AttributeField("originated-amount-earned")
     originated_calls_count = AttributeField("originated-calls-count")
-    originated_successful_calls_count = AttributeField("originated-successful-calls-count")
+    originated_successful_calls_count = AttributeField(
+        "originated-successful-calls-count"
+    )
     originated_calls_duration = AttributeField("originated-calls-duration")
     originated_billing_duration = AttributeField("originated-billing-duration")
     originated_first_call_at = AttributeField("originated-first-call-at")
@@ -32,15 +34,13 @@ class Invoice(BaseModel):
     terminated_amount_spent = AttributeField("terminated-amount-spent")
     terminated_amount_earned = AttributeField("terminated-amount-earned")
     terminated_calls_count = AttributeField("terminated-calls-count")
-    terminated_successful_calls_count = AttributeField("terminated-successful-calls-count")
+    terminated_successful_calls_count = AttributeField(
+        "terminated-successful-calls-count"
+    )
     terminated_calls_duration = AttributeField("terminated-calls-duration")
     terminated_billing_duration = AttributeField("terminated-billing-duration")
     terminated_first_call_at = AttributeField("terminated-first-call-at")
     terminated_last_call_at = AttributeField("terminated-last-call-at")
 
     def creatable_fields(self):
-        return [
-            "account",
-            "start_date",
-            "end_date"
-        ]
+        return ["account", "start_date", "end_date"]
