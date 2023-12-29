4adc0b02df7ee86f46cf4484ca9a4c486e5310e3
Dmitry <dmitry.sinina@onat.edu.ua>
2023-12-29 19:13:40 +0200

    fix

--- yeti_switch_api/orm/billing/__init__.py
+++ yeti_switch_api/orm/billing/__init__.py
@@ -1,5 +1,5 @@
 from .contact import Contact  # noqa: F401
-from .invoice.py import Invoice  # noqa: F401
+from .invoice import Invoice  # noqa: F401
 from .invoice_originated_destination import InvoiceOriginatedDestination  # noqa: F401
 from .invoice_originated_network import InvoiceOriginatedNetwork  # noqa: F401
 from .invoice_terminated_destination import InvoiceTerminatedDestination  # noqa: F401
