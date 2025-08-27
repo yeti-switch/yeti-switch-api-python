48c263c2d3d19c6767a09f700c1f7d5dd24f3aa8
Dmitry Sinina <dmitry.s@yeti-switch.org>
2025-08-27 19:08:48 +0300

    more attributes for customer auth (#13)
    * more attributes for customer auth

--- yeti_switch_api/orm/customers_auth.py
+++ yeti_switch_api/orm/customers_auth.py
@@ -8,6 +8,8 @@ class CustomersAuth(BaseModel):
 
     name = AttributeField("name")
     enabled = AttributeField("enabled")
+    reject_calls = AttributeField("reject-calls")
+
     src_numberlist = RelationField("src-numberlist")
     dst_numberlist = RelationField("dst-numberlist")
     customer = RelationField("customer")
@@ -15,19 +17,39 @@ class CustomersAuth(BaseModel):
     rateplan = RelationField("rateplan")
     routing_plan = RelationField("routing-plan")
     gateway = RelationField("gateway")
+
     pop = RelationField("pop")
     ip = AttributeField("ip")
+
     src_prefix = AttributeField("src-prefix")
+    src_number_min_length = AttributeField("src-number-min-length")
+    src_number_max_length = AttributeField("src-number-max-length")
     dst_prefix = AttributeField("dst-prefix")
+    dst_number_min_length = AttributeField("dst-number-min-length")
+    dst_number_max_length = AttributeField("dst-number-max-length")
+
     x_yeti_auth = AttributeField("x-yeti-auth")
+
+    uri_domain = AttributeField("uri-domain")
+    from_domain = AttributeField("from-domain")
+    to_domain = AttributeField("to-domain")
+
+    capacity = AttributeField("capacity")
+    cps_limit = AttributeField("cps-limit")
 
     tag_action = RelationField("tag-action")
     tag_action_value = AttributeField("tag-action-value")
+
+    src_rewrite_rule = AttributeField("src-rewrite-rule")
+    src_rewrite_result = AttributeField("src-rewrite-result")
+    dst_rewrite_rule = AttributeField("dst-rewrite-rule")
+    dst_rewrite_result = AttributeField("dst-rewrite-result")
 
     def creatable_fields(self):
         return [
             "name",
             "enabled",
+            "reject-calls",
             "src-numberlist",
             "dst-numberlist",
             "customer",
@@ -38,10 +60,23 @@ class CustomersAuth(BaseModel):
             "pop",
             "ip",
             "src-prefix",
+            "src-number-min-length",
+            "src-number-max-length",
             "dst-prefix",
+            "dst-number-min-length",
+            "dst-number-max-length",
             "x-yeti-auth",
+            "uri-domain",
+            "from-domain",
+            "to-domain",
+            "capacity",
+            "cps-limit",
             "tag-action",
             "tag-action-value",
+            "src-rewrite-rule",
+            "src-rewrite-result",
+            "dst-rewrite-rule",
+            "dst-rewrite-result",
         ]
 
     def updatable_fields(self):
